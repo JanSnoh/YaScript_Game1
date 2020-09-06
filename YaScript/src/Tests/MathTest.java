f24fc70de96e7e94938eb259d5c971849dbae34e
kajus-park <park.kajus@gmail.com>
2020-09-06 18:21:39 +0200

    Added Test directory for Testing Stuff and to write short scripts in.

---
+++ YaScript/src/Tests/MathTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import math.*;
+import org.junit.jupiter.api.Test;
+
+public class MathTest {
+    @Test
+    void testInverse() {
+        Matrix a = new Matrix(1, 2, 1, 0);
+        System.out.println(a);
+        Matrix b = a.inverse();
+        System.out.println(b);
+        System.out.println(a.mult(b));
+    }
+}
